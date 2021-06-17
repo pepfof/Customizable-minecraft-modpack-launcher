42e9ba11add38256bdbc02c9a4366f7845ddabd0
pepfof <44321650+pepfof@users.noreply.github.com>
2021-06-18 01:38:02 +0300

    Update Custom.py

--- Custom.py
+++ Custom.py
@@ -8,7 +8,7 @@ Forge_version_name = "1.12.2-forge-14.23.5.2855"    #the folder forge generates 
 Is_below_1_13 = 1                                   #set to 1 if running 1.12.2. the launcher will ONLY work with latest forge 1.12.2 releases
 Min_Mem = 2048                                      #suggested memory allocation in the launcher
 
-Source_URL = "https://..."                          #url of the distributor folder
-Server_Autoconnect = 1                              #whether to autoconnect on launch to:
-Server_IP = "..."                                   #the server on this ip
-Server_port = "..."                                 #at this port
+Source_URL = "https://pepfof.com/minecraft/"        #url of the distributor folder
+Server_Autoconnect = 0                              #whether to autoconnect on launch to:
+Server_IP = ""                                      #the server on this ip
+Server_port = ""                                    #at this port
