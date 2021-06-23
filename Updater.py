98aaf0258147d8be0120cec9b3bfece7411d76f9
pepfof <artemiodro@gmail.com>
2021-06-23 11:10:50 +0300

    Compile update

--- Updater.py
+++ Updater.py
@@ -9,6 +9,7 @@ from getpass import getuser
 from datetime import datetime
 import Custom
 
+
 def resource_path(relative_path):
     try:
         base_path = sys._MEIPASS
@@ -19,12 +20,13 @@ def resource_path(relative_path):
 
 
 class Ui_Dialog(object):
-    
+
     def setupUi(self, Dialog):
         Dialog.setObjectName(Custom.Updater_title)
         Dialog.setFixedSize(423, 311)
         icon = QIcon()
-        icon.addPixmap(QPixmap(resource_path("icon.ico")), QIcon.Normal, QIcon.Off)
+        icon.addPixmap(QPixmap(resource_path("icon.ico")),
+                       QIcon.Normal, QIcon.Off)
         Dialog.setWindowIcon(icon)
         self.pushButton = QPushButton(Dialog)
         self.pushButton.setGeometry(QRect(310, 10, 101, 44))
@@ -58,20 +60,22 @@ class Ui_Dialog(object):
         QMetaObject.connectSlotsByName(Dialog)
 
         self.log = " "
-        
+
     def retranslateUi(self, Dialog):
         _translate = QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", Custom.Updater_title))
         self.aboutbutton.setToolTip(_translate("Dialog", "About"))
         self.label.setText(_translate("Dialog", "Minecraft Directory:"))
-        self.pushButton.setToolTip(_translate("Dialog", "Shift+click to force download all"))
+        self.pushButton.setToolTip(_translate(
+            "Dialog", "Shift+click to force download all"))
         self.pushButton.setText(_translate("Dialog", "Update!"))
         self.aboutbutton.setText(_translate("Dialog", "?"))
         self.dirselect.setText(_translate("Dialog", "..."))
-    
+
     def knopka(self):
         icon = QIcon()
-        icon.addPixmap(QPixmap(resource_path("icon.ico")), QIcon.Normal, QIcon.Off)
+        icon.addPixmap(QPixmap(resource_path("icon.ico")),
+                       QIcon.Normal, QIcon.Off)
         msgBox = QMessageBox()
         msgBox.setText(f'''<a href = "https://github.com/pepfof/Customizable-minecraft-modpack-launcher"><h2>Customizable minecraft modpack launcher</h2></a> by bopchik and pepfof<br>licensed under the <a href = "https://github.com/pepfof/Customizable-minecraft-modpack-launcher/blob/main/LICENSE">BSD 2-clause license</a><br><br>
         Written using <a href = "https://gitlab.com/JakobDev/minecraft-launcher-lib">Minecraft Launcher Lib by JakobDev</a><br> licensed under the <a href = "https://gitlab.com/JakobDev/minecraft-launcher-lib/-/blob/master/LICENSE">BSD 2-clause license</a><br><br>
@@ -86,16 +90,17 @@ class Ui_Dialog(object):
 
     def updateLog(self, input):
         original = self.log
-        self.log = datetime.now().strftime("[%H:%M:%S] ") + input + "\n" + original
+        self.log = datetime.now().strftime(
+            "[%H:%M:%S] ") + input + "\n" + original
         with open("./updater_latest.log", 'w') as file:
             file.write(self.log)
         self.textBrowser.setPlainText(self.log)
 
-    def runLongTask(self):  
+    def runLongTask(self):
         self.thread = QThread()
         self.worker = Worker()
         self.worker.moveToThread(self.thread)
-        self.thread.started.connect(self.worker.run)        
+        self.thread.started.connect(self.worker.run)
         self.worker.finished.connect(self.thread.quit)
         self.worker.finished.connect(self.worker.deleteLater)
         self.thread.finished.connect(self.thread.deleteLater)
@@ -103,21 +108,23 @@ class Ui_Dialog(object):
         self.worker.lineEdit_set.connect(self.lineEdit.setEnabled)
         self.worker.dirselect.connect(self.dirselect.setEnabled)
         self.worker.progressBar.connect(self.progressBar.setProperty)
-        self.worker.logging.connect(self.updateLog)      
+        self.worker.logging.connect(self.updateLog)
         self.thread.start()
 
     def dirselecting(self):
-         self.lineEdit.setText(QFileDialog.getExistingDirectory(None, "Open Directory", self.lineEdit.text(), QFileDialog.ShowDirsOnly | QFileDialog.DontResolveSymlinks))
+        self.lineEdit.setText(QFileDialog.getExistingDirectory(None, "Open Directory", self.lineEdit.text(
+        ), QFileDialog.ShowDirsOnly | QFileDialog.DontResolveSymlinks))
+
 
 class Worker(QObject):
-    
+
     finished = pyqtSignal()
     pushButton = pyqtSignal(int)
     lineEdit_set = pyqtSignal(int)
     dirselect = pyqtSignal(int)
     progressBar = pyqtSignal(str, int)
     logging = pyqtSignal(str)
-    
+
     def progressbar(self, counter, all):
         self.progressBar.emit("maximum", all)
         self.progressBar.emit("value", counter)
@@ -138,15 +145,16 @@ class Worker(QObject):
             self.logging.emit(f'No directory selected!')
             self.finished.emit()
             return 0
-        
+
         fpath = ui.lineEdit.text()
 
         path_to_txtn = join(path, 'dir.txt')
         with open(path_to_txtn, 'w') as file:
-                file.write(ui.lineEdit.text())
+            file.write(ui.lineEdit.text())
         if(force):
             if(not check):
-                self.logging.emit(f'Warning!\nForce redownloading will\ndelete all previously downloaded content!\nShift-click "Update!" again to continue.\n\n\n\n\n')
+                self.logging.emit(
+                    f'Warning!\nForce redownloading will\ndelete all previously downloaded content!\nShift-click "Update!" again to continue.\n\n\n\n\n')
                 check = True
                 self.dirselect.emit(1)
                 self.lineEdit_set.emit(1)
@@ -156,29 +164,31 @@ class Worker(QObject):
             if(check):
                 check = False
 
-        req = get(Custom.Source_URL + "mine.txt")            
+        req = get(Custom.Source_URL + "mine.txt")
         files_new = set(req.text.split('\n'))
         files_new.remove('')
         files_new = {a[1:].replace('\\', '/') for a in files_new}
         if(force):
             for i in files_new:
-                if(exists(join(fpath,i))):
-                    remove(join(fpath,i))
-        self.logging.emit(f'Checking for updates...')        
+                if(exists(join(fpath, i))):
+                    remove(join(fpath, i))
+        self.logging.emit(f'Checking for updates...')
         req = get(Custom.Source_URL + "/dirs.txt")
-        all_dirs = {i[1:].replace('\\', '/') for i in req.text.split('\n') if not exists(join(fpath,i[1:]))}
+        all_dirs = {i[1:].replace(
+            '\\', '/') for i in req.text.split('\n') if not exists(join(fpath, i[1:]))}
         req = get(Custom.Source_URL + "remv.txt")
-        files_delete = {i[1:].replace('\\', '/') for i in req.text.split('\n') if i != '' and exists(join(fpath,i[1:]))}
+        files_delete = {i[1:].replace(
+            '\\', '/') for i in req.text.split('\n') if i != '' and exists(join(fpath, i[1:]))}
         for i in files_delete:
-            remove(join(fpath,i))
+            remove(join(fpath, i))
         for i in all_dirs:
-            if(not exists(join(fpath,i))):
-                makedirs(join(fpath,i))
+            if(not exists(join(fpath, i))):
+                makedirs(join(fpath, i))
         if(not force):
-            files_new = {i for i in files_new if not exists(join(fpath,i))}
+            files_new = {i for i in files_new if not exists(join(fpath, i))}
         counter = 0
         for i in files_new:
-            with open(join(fpath,i), 'wb') as f:
+            with open(join(fpath, i), 'wb') as f:
                 ufr = get(f"{Custom.Source_URL}{i}")
                 f.write(ufr.content)
                 counter += 1
@@ -186,15 +196,17 @@ class Worker(QObject):
                 self.logging.emit(f'Download {i}')
 
         self.logging.emit(f"Done updating {Custom.Modpack_name}!")
-        self.progressbar(1,0)
+        self.progressbar(1, 0)
         self.dirselect.emit(1)
         self.lineEdit_set.emit(1)
         self.pushButton.emit(1)
         self.finished.emit()
 
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    Dialog = QDialog(None, Qt.WindowSystemMenuHint | Qt.WindowTitleHint | Qt.WindowCloseButtonHint)
+    Dialog = QDialog(None, Qt.WindowSystemMenuHint |
+                     Qt.WindowTitleHint | Qt.WindowCloseButtonHint)
     ui = Ui_Dialog()
     ui.setupUi(Dialog)
     Dialog.show()
@@ -202,7 +214,7 @@ if __name__ == "__main__":
     check = False
     if(not exists(path)):
         mkdir(path)
-    
+
     default_name = f'C:\\Users\\{getuser()}\\AppData\\Roaming\\.minecraft'
     path_to_txtn = join(path, 'dir.txt')
     if(exists(path_to_txtn)):
