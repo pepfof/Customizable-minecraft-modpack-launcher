2b39864accc33ccc6302aeda506594e6373cddc6
pepfof <artemiodro@gmail.com>
2021-06-26 17:25:40 +0300

    increase min mem alloc

--- Custom.py
+++ Custom.py
@@ -13,7 +13,7 @@ Launcher_folder_path = ".memcraft"                  #this is relative to the use
 Forge_version = "1.12.2-14.23.5.2855"               #exact forge version names can be found on the forge website
 Forge_version_name = "1.12.2-forge-14.23.5.2855"    #the folder forge generates in the minecraft/versions folder, as well as the name it shows in the vanilla minecraft launcher. Find this one out yourself
 Is_below_1_13 = 1                                   #set to 1 if running 1.12.2. the launcher will ONLY work with latest forge 1.12.2 releases
-Min_Mem = 2048                                      #suggested memory allocation in the launcher
+Min_Mem = 2560                                      #suggested memory allocation in the launcher
 
 Source_URL = "https://pepfof.com/minecraft/"        #url of the distributor folder
 Server_Autoconnect = 1                              #whether to autoconnect on launch to:
